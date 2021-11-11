b2cd74596e8c95a63d8bf7826b5d8a35db5a9332
DominikWawak <dominikwawak@gmail.com>
2021-11-11 18:05:11 +0000

    Exercise1

--- MoviesAPI/api/movies/index.js
+++ MoviesAPI/api/movies/index.js
@@ -52,4 +52,6 @@ router.post('/:id/reviews', (req, res) => {
     }
 });
 
+
+
 export default router;
--- MoviesAPI/index.js
+++ MoviesAPI/index.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 
+import genresRouter from './api/genres'
+
 dotenv.config();
 
 const app = express();
@@ -11,6 +13,7 @@ const port = process.env.PORT;
 
 app.use('/api/movies', moviesRouter);
 
+app.use('/api/genres', genresRouter);
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
