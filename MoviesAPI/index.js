eef5447cb89cafcf12ce488d9214a867eea84c9e
DominikWawak <dominikwawak@gmail.com>
2021-12-02 20:01:40 +0000

    fix

--- MoviesAPI/index.js
+++ MoviesAPI/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import bodyParser from 'body-parser';
 import moviesRouter from './api/movies';
 import './db'
 import './seedData'
@@ -7,37 +8,28 @@ import usersRouter from './api/users';
 import session from 'express-session';
 import authenticate from './authenticate';
 import passport from './authenticate';
-
-import genresRouter from './api/genres'
-
-dotenv.config();
-
-const app = express();
-app.use(express.json());
-
-const port = process.env.PORT;
-
-// Add passport.authenticate(..)  to middleware stack for protected routes​
-app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter)
-
-app.use('/api/genres', genresRouter);
-
-app.use(passport.initialize());
-
-app.listen(port, () => {
-  console.info(`Server running at ${port}`);
-});
-
 const errHandler = (err, req, res, next) => {
   /* if the error in development then send stack trace to display whole error,
   if it's in production then just send error message  */
   if(process.env.NODE_ENV === 'production') {
     return res.status(500).send(`Something went wrong!`);
   }
-  res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+  res.status(500).send(`Hey!! You caught the error :+1::+1:. Here's the details: ${err.stack} `);
 };
-
-
+import genresRouter from './api/genres'
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+// Add middleware
+app.use(express.json());
+app.use(passport.initialize());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter)
+app.use('/api/genres', genresRouter);
 app.use('/api/users', usersRouter);
-
 app.use(errHandler);
+// Start server
+app.listen(port, () => {
+  console.info(`Server running at ${port}`);
+});
